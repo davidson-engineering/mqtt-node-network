9afa1dc74788d2ebf69b1a834caf1609c6fafe30
Matthew Davidson <admin@davidson.engineering>
2024-03-16 20:07:57 -0700

    fixed logger config kw arg

--- src/mqtt_node_network/initialize.py
+++ src/mqtt_node_network/initialize.py
@@ -10,7 +10,7 @@ import logging
 def initialize(
     config: Union[str, dict] = None,
     secrets: Union[str, dict] = None,
-    logging_config: Union[str, dict] = None,
+    logger: Union[str, dict] = None,
 ) -> tuple[dict, logging.Logger]:
     from mqtt_node_network.configure import MQTTBrokerConfig
     from mqtt_node_network.configure import load_secrets
@@ -18,7 +18,7 @@ def initialize(
     config = build_config(config)
     secrets = secrets or config.get("secrets_filepath")
     secrets = load_secrets(secrets, ["MQTT_BROKER_USERNAME", "MQTT_BROKER_PASSWORD"])
-    logger = setup_logging(logging_config)
+    logger = setup_logging(logger)
 
     PROMETHEUS_ENABLE = config["mqtt"]["node_network"].get("enable_prometheus_server")
     PROMETHEUS_PORT = config["mqtt"]["node_network"].get("prometheus_port")
