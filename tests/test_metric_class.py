189e46636655ca3d7c8b3b1aba004a23e3e5b92f
Matthew Davidson <122948876+generalmattza@users.noreply.github.com>
2024-04-23 16:18:59 -0700

    added testing for metric class

---
+++ tests/test_metric_class.py
@@ -0,0 +1,23 @@
+from mqtt_node_network.client import Metric
+
+
+def test_metric():
+    # test item assignment
+    metric = Metric(
+        measurement="temperature",
+        fields={"value": 25.0},
+        time=1629000000,
+        tags={"location": "bedroom"},
+    )
+    assert metric["measurement"] == "temperature"
+    assert metric["fields"] == {"value": 25.0}
+    assert metric["time"] == 1629000000
+    assert metric["tags"] == {"location": "bedroom"}
+    metric["measurement"] = "humidity"
+    assert metric["measurement"] == "humidity"
+    metric["fields"] = {"value": 50.0}
+    assert metric["fields"] == {"value": 50.0}
+    metric["time"] = 1629000001
+    assert metric["time"] == 1629000001
+    metric["tags"] = {"location": "kitchen"}
+    assert metric["tags"] == {"location": "kitchen"}
